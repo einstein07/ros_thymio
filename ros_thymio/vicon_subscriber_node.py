3d4514d1364ba9a1b8a0922738718f99b324d560
Sindiso <mkhsin035@myuct.ac.za>
2024-11-19 11:59:46 +0100

    Changed callback to accommodate list of all object in arena

--- ros_thymio/vicon_subscriber_node.py
+++ ros_thymio/vicon_subscriber_node.py
@@ -1,24 +1,56 @@
 import rclpy
 from rclpy.node import Node
-
-from vicon_interfaces.msg import Position
+from message_filters import Subscriber, TimeSynchronizer
+from vicon_receiver.msg import Position
+from vicon_receiver.msg import PositionList
 
 
 class ViconSubscriber(Node):
 
     def __init__(self):
-        super().__init__('minimal_subscriber')
+        super().__init__('vicon_subscriber')
+        self.declare_parameter('my_id', rclpy.Parameter.Type.STRING)
         self.subscription = self.create_subscription(
-            Position,
-            '/vicon/sindiso/sindiso',
+            PositionList,
+            '/vicon/default/data',
             self.listener_callback,
             10)
-        self.subscription  # prevent unused variable warning
+
+
+        """self.declare_parameter('list_of_topics', rclpy.Parameter.Type.STRING_ARRAY)
+        self.id, self.topic_list = self.configure_params()
+        self.subscriptions = []
+        for topic in self.topic_list:
+            self.subscriptions.append(
+                                        Subscriber(
+                                        self,
+                                        Position,
+                                        topic
+                                        )
+            )
+            self.get_logger().info('successfully subscribed to topic "%s"' % topic)
+        self.timer = self.create_timer(1, self.TimerCallback)
+        queue_size = 10
+        self.sync = TimeSynchronizer(self.subscriptions, queue_size)
+        self.sync.registerCallback(self.SyncCallback)
+
+    def configure_params(self):
+        id = self.get_parameter('my_id')
+        topic_list = self.get_parameter('list_of_topics')
+        self.get_logger().info("my id: %s, topic-list[]: %s" %
+                               (str(id.value),
+                                str(topic_list.value),))
+
+        return id, topic_list"""
+
+    #def SyncCallback(self, msg):
+
 
     def listener_callback(self, msg):
-        self.get_logger().info('subject "%s" with segment %s:' %(msg.subject_name, msg.segment_name))
-        self.get_logger().info('I heard translation in x, y, z: "%f", "%f", "%f"' % (msg.x_trans, msg.y_trans, msg.z_trans))
-        self.get_logger().info('I heard rotation in x, y, z, w: "%f", "%f", "%f", "%f": ' % (msg.x_rot, msg.y_rot, msg.z_rot, msg.w))
+        for i in range(msg.n):
+            self.get_logger().info('subject "%s" with segment %s:' %(msg.position[i].subject_name, msg.position[i].segment_name))
+            self.get_logger().info('I heard translation in x, y, z: "%f", "%f", "%f"' % (msg.position[i].x_trans, msg.position[i].y_trans, msg.position[i].z_trans))
+            self.get_logger().info('I heard rotation in x, y, z, w: "%f", "%f", "%f", "%f": ' % (msg.position[i].x_rot, msg.position[i].y_rot, msg.position[i].z_rot, msg.position[i].w))
 
 def main(args=None):
     rclpy.init(args=args)
